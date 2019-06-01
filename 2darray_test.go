5888612e183ba1ab296a3633f9073d28f342f023
Anders Fylling <anders@nordic.email>
2019-06-01 13:36:48 +0200

    large array is a Go issue, not ikeapack - commented out large test

--- 2darray_test.go
+++ 2darray_test.go
@@ -8,18 +8,20 @@ import (
 )
 
 // Causes error: too much data in section SBSS / SNOPTRBSS
-type HolderLarge struct {
-	vals [50000000][10]uint16
-	buffer    *bytes.Buffer `ikea:"-"`
-}
-
-func (h *HolderLarge) Init() {
-	h.buffer = new(bytes.Buffer)
-}
-
-func (h *HolderLarge) compress() error {
-	return Pack(h.buffer, h)
-}
+// this is a issue with Go: https://github.com/golang/go/issues/17378
+// This test is currently removed, until go fixes this issue.
+//type HolderLarge struct {
+//	vals [100000000][10]uint16
+//	buffer    *bytes.Buffer `ikea:"-"`
+//}
+//
+//func (h *HolderLarge) Init() {
+//	h.buffer = new(bytes.Buffer)
+//}
+//
+//func (h *HolderLarge) compress() error {
+//	return Pack(h.buffer, h)
+//}
 
 type HolderSmall struct {
 	vals [10][10]uint16
@@ -35,19 +37,19 @@ func (h *HolderSmall) compress() error {
 }
 
 
-var _holderL HolderLarge
-var _holderS HolderLarge
+// var _holderL HolderLarge
+var _holderS HolderSmall
 
 func init() {
-	_holderL.Init()
+	//_holderL.Init()
 	_holderS.Init()
 
-	for i := range _holderL.vals {
-		for j := range _holderL.vals[i] {
-			v := rand.Uint32()
-			_holderL.vals[i][j] = uint16(v)
-		}
-	}
+	//for i := range _holderL.vals {
+	//	for j := range _holderL.vals[i] {
+	//		v := rand.Uint32()
+	//		_holderL.vals[i][j] = uint16(v)
+	//	}
+	//}
 
 	for i := range _holderS.vals {
 		for j := range _holderS.vals[i] {
@@ -57,26 +59,26 @@ func init() {
 	}
 }
 
-func TestLarge2DArrayPackingAndUnpacking(t *testing.T) {
-	if err := _holderL.compress(); err != nil {
-		t.Fatal(err)
-	}
-
-	h2 := HolderLarge{}
-	h2.Init()
-	err := Unpack(_holderL.buffer, &h2)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	for i := range _holderL.vals {
-		for j := range _holderL.vals[i] {
-			if _holderL.vals[i][j] != h2.vals[i][j] {
-				t.Fatal(fmt.Sprintf("diff values. At vals[%d][%d]", i, j))
-			}
-		}
-	}
-}
+//func TestLarge2DArrayPackingAndUnpacking(t *testing.T) {
+//	if err := _holderL.compress(); err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	h2 := HolderLarge{}
+//	h2.Init()
+//	err := Unpack(_holderL.buffer, &h2)
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	for i := range _holderL.vals {
+//		for j := range _holderL.vals[i] {
+//			if _holderL.vals[i][j] != h2.vals[i][j] {
+//				t.Fatal(fmt.Sprintf("diff values. At vals[%d][%d]", i, j))
+//			}
+//		}
+//	}
+//}
 
 func TestSmall2DArrayPackingAndUnpacking(t *testing.T) {
 	if err := _holderS.compress(); err != nil {
